ae928c2c8f2710d8fcdcb4ebbd40c4567c5b95c4
rartigues2019 <rartigues2019@inf.udec.cl>
2021-10-03 19:04:01 -0300

    components basic listo

--- react-crud/src/App.js
+++ react-crud/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import AddTutorial from "./components/add-tutorial.component";
+import Tutorial from "./components/tutorial.component";
+import TutorialsList from "./components/tutorials-list.component";
+
 class App extends Component {
   render() {
     return (
